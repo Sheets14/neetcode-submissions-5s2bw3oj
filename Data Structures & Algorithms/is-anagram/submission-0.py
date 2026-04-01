cc026b0ee53d703df2f7ab8f29aeb47298c58cec
Sheets14 <114384739+Sheets14@users.noreply.github.com>
2026-04-01 10:05:56 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # if len(s)!=len(t):
+        #     return False
+        # countS,countT = {},{}
+        return collections.Counter(s)==collections.Counter(t)
